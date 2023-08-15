b704c2c3f83d100daee740556fe0448bcb4998b7
AbdulQadir98 <abdul.qadirv98@gmail.com>
2023-08-15 19:40:11 +0530

    init backend

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+
+var corsOptions = {
+  origin: "http://localhost:3000"
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+// const db = require("./models");
+
+// db.sequelize.sync()
+//   .then(() => {
+//     console.log("Connected with database...");
+//   })
+//   .catch((err) => {
+//     console.log("Failed to sync db: " + err.message);
+//   });
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Orion" });
+});
+
+// require("./routes/user.routes")(app);
+
+const PORT = process.env.DB_PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
