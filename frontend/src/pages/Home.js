6c19045f926f831718c71f1494e3758bb991003a
AbdulQadir98 <abdul.qadirv98@gmail.com>
2023-08-16 19:50:42 +0530

    booking home ui

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../styles/header.css'
+
+const Home = () => {
+    return ( 
+      <>  
+        {/* header */}
+        <div className='header'>
+            <div className='bg-gray-100'>qwe</div>
+            <div>Orion</div>
+        </div> 
+
+        {/* tabs */}
+        <div className='tabs'></div>  
+
+        <div></div>
+      </>
+     );
+}
+ 
+export default Home;
