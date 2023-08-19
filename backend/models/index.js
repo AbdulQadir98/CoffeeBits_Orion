80cec951d39f89c4ff094dba4dbfe037d9881e75
msmohamed <emails3msm@gmail.com>
2023-08-20 01:22:20 +0530

    configured the application with docker compose

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,7 +2,7 @@ const dbConfig = require("../config/config.js");
 
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: "localhost",
+  host: dbConfig.HOST,
   dialect: dbConfig.dialect,
   operatorsAliases: 0,
 
