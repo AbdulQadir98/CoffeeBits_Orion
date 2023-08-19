b62f1e0813a3ff7f0fb26e28921624c365afb886
Nisaf Ahamed <87429268+nisafaha@users.noreply.github.com>
2023-08-19 11:28:36 +0530

    form added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
+import Info from "./pages/Info";
 
 
 function App(){
@@ -10,6 +11,7 @@ function App(){
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/info" element={<Info />} />
         </Routes>
       </Router>
     </>
