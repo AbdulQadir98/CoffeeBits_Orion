bf3563f4e1a2bf67254d764435ff29a04e550a6a
AbdulQadir98 <abdul.qadirv98@gmail.com>
2023-08-19 12:36:27 +0530

    added checkout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Info from "./pages/Info";
+import Checkout from "./pages/Checkout";
 import Flight from "./pages/Flight";
 
 function App(){
@@ -12,6 +13,7 @@ function App(){
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/info" element={<Info />} />
+          <Route path="/checkout" element={<Checkout />} />
           <Route path="/flight" element={<Flight />} />
         </Routes>
       </Router>
